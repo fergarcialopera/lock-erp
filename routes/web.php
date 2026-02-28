db0be5f5c1b00baf86fc355a1fd697eb9ad3e4e6
fergarcialopera <fergarcialopera@gmail.com>
2026-02-28 10:33:21 +0100

    feat: Implement ForceJsonResponse middleware and define initial root API endpoint.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+    'message' => 'Locker ERP API',
+    'version' => '1.0.0',
+    'status' => 'running'
+    ]);
 });
